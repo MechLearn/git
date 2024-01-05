a2b8ea9104a470aa79790929daa4037f38df746d
David <mechdev96@gmail.com>
2024-01-05 18:22:36 -0500

    2 update

---
+++ Coding_Projects/Dict.py
@@ -0,0 +1,15 @@
+d = {'a': 1 , 'b' : 2, "c": 3}
+
+print(d['a'])
+
+d['d']= 4
+
+print(d)
+
+d.values()
+
+d.update()
+
+d.remove    
+
+
--- Coding_Projects/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-T= (1,2,True, "Hey")
-l = ['blue', 'yellow', 'green', 'red', 'purple']
-
-for v1,v2 in zip(T,l):
-    print(v1,v2)
