037112b4f9e35e49d907639da0d3eedabf172680
David <mechdev96@gmail.com>
2024-01-06 07:24:47 -0500

    5 Update

---
+++ POO/Class.py
@@ -0,0 +1,14 @@
+class MyClass:
+    
+    a ='hello'
+    b = 123
+    
+    
+    def func(self):
+        print(self.a,self.b)
+        
+ ##Just can access with method       
+myClass = MyClass()  
+
+myClass.func()
+
---
+++ POO/tempCodeRunnerFile.py
@@ -0,0 +1,14 @@
+class MyClass:
+    
+    a ='hello'
+    b = 123
+    
+    
+    def func(self):
+        print(self.a)
+        
+ ##Just can access with method       
+myClass = MyClass()  
+
+myClass.func()
+
